7c151130faf94bd122b07062b725128a3a035e28
AmandaRH07 <amandarafahass13@gmail.com>
2022-01-04 11:56:40 -0300

    Person Implementation mock

--- 00_RestAspNet_Scaffold/RestAspNet/Startup.cs
+++ 00_RestAspNet_Scaffold/RestAspNet/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestAspNet.Services.Implementations;
 
 namespace RestAspNet
 {
@@ -28,6 +22,9 @@ namespace RestAspNet
         {
 
             services.AddControllers();
+
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestAspNet", Version = "v1" });
